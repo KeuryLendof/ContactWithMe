c0c4b04f1844bec0f9996c6f449eca09275188ee
KeuryLendof <lendofkeury@gmail.com>
2023-03-03 17:52:07 -0400

    redesing code 3

--- app.js
+++ app.js
@@ -60,13 +60,73 @@ const apiSkills=[
         experiencia: '+2 años'
     },
     {
+        imagen: './assets/skills/microsoftsqlserver.svg',
+        nombre: 'Sql Server',
+        experiencia: '+2 años'
+    },
+    {
+        imagen: './assets/skills/oracle.svg',
+        nombre: 'Oracle',
+        experiencia: '+2 años'
+    },
+    {
+        imagen: './assets/skills/sqlite.svg',
+        nombre: 'Sql Lite',
+        experiencia: '3 años'
+    },
+    {
         imagen: './assets/skills/firebase.svg',
         nombre: 'Firebase',
         experiencia: '1 año'
     },
 ]
+const apiProjects=[
+    {
+        imagen: './assets/proyectos/top-100-cryptocurrencies.vercel.app.jpeg',
+        nombre: 'top-100-cryptocurrencies',
+        url: 'https://top-100-cryptocurrencies.vercel.app/',
+        experiencia: ['React','Fetch Api'],
+        descripcion: 'ApiCrypto es una web que ofrece un análisis fundamental del mercado de criptomonedas, seguiendo el precio, el volumen y la capitalización bursátil.'
+    },
+    {
+        imagen: './assets/proyectos/shabless.vercel.app.jpeg',
+        nombre: 'Shabless E-Commerce',
+        url: 'https://shabless.vercel.app/',
+        experiencia: ['React','Firebase'],
+        descripcion: 'Shabless es un ecommerce de accesorios para aquellos que buscan complementar su estilo personal con productos únicos y de calidad.'
+    },
+    {
+        imagen: './assets/proyectos/text-to-speech-gamma.vercel.app.jpeg',
+        nombre: 'Text to Speech',
+        url: 'https://text-to-speech-gamma.vercel.app/',
+        experiencia: ['HTML',' CSS', 'Javascript'],
+        descripcion: 'Text to Speech es un proyecto el cual su nombre lo describe completamente, en este podras poner tu texto y escucharlo y vice-versa.'
+    },
+    {
+        imagen: './assets/proyectos/argerlin-rent-a-car.png',
+        nombre: 'Argerlin Rent A Car',
+        url: 'https://github.com/KeuryLendof/Argerlin-Car-Rental',
+        experiencia: ['HTML',' CSS', 'Javascript'],
+        descripcion: ''
+    },
+    {
+        imagen: './assets/proyectos/geofig.png',
+        nombre: 'GeoFig',
+        url: 'https://github.com/KeuryLendof/GeoFig',
+        experiencia: ['React'],
+        descripcion: 'GeoFig es un sitio en el cual podras calcular el area de varias figuras geometricas de 2,3,5,6 y mas dimensiones, de manera rápida y sencilla.'
+    },
+    {
+        imagen: './assets/proyectos/web-trailers.png',
+        nombre: 'Web-Trailers',
+        url: 'https://github.com/KeuryLendof/Programacion-Web-Trailers',
+        experiencia: ['TypeScript','C#','.NET','CSS'],
+        descripcion: 'Web-Trailers es un proyecto donde se podran ver trailers, reseñas, actores y la valoraciones del publico.'
+    }
+]
 
 const skillsContainer = document.getElementById('skills');
+const projectsContainer = document.getElementById('projects');
 
 apiSkills.forEach(s=>{
 
@@ -83,5 +143,23 @@ apiSkills.forEach(s=>{
     // document.getElementById('experiencia-skill').innerText = s.experiencia;
 
     skillsContainer.innerHTML += content;
+})
+apiProjects.map(function(p){
+    
+    const content =`
+        <article>
+            <figure>
+                <img src="${p.imagen}" alt="">
+            </figure>
+            <a href="${p.url}" target="_blank">${p.nombre} <i class="fa fa-arrow-right"></i></a>
+            <section class="chips-container">
+                ${p.experiencia.map(function(e){
+                    return '<small>'+e+'</small>'
+                })}
+            </section>
+            <p>${p.descripcion}</p>
+        </article>
+    `
+    projectsContainer.innerHTML += content;
 })
 
