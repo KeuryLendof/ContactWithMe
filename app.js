1a8ab693c86f2f5c893feb3ee7db3a41afe00ed5
KeuryLendof <lendofkeury@gmail.com>
2023-03-10 16:51:14 -0400

    redesing code 3.4

--- app.js
+++ app.js
@@ -169,9 +169,12 @@ function textLoad(){
     setTimeout(()=>{
         text.textContent = 'Web Developer'
     },7000)
+    setTimeout(()=>{
+        text.textContent = 'Front-end Developer'
+    },14000)
 }
 textLoad()
-setInterval(textLoad,14000)
+setInterval(textLoad,21000)
 
 links.forEach((l)=>{
     l.addEventListener('mouseover',()=>{
@@ -254,3 +257,11 @@ apiSkills.forEach(s=>{
 //     projectsContainer.innerHTML += content;
 // })
 
+var device = navigator.userAgent;
+
+if(device.match(/Iphone/i) || device.match(/Android/i) || device.match(/Ipod/i) || device.match(/J2ME/i)|| device.match(/       BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i)|| device.match(/CriOS/i)){
+   innerCursor.classList.add('grownone')
+    outerCursor.classList.add('grownone')
+}else{
+    /*Soy una pc ninguna accion*/
+}
